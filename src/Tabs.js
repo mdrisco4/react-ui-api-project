304f3a7702f17e38aab0f1ceede11c73e488bc31
michaeldriscoll <michaelndriscoll81@gmail.com>
2019-12-14 21:43:10 -0500

    exploring other ideas and apis for project

--- src/Tabs.js
+++ src/Tabs.js
@@ -8,8 +8,8 @@ class Tabs1 extends Component {
     // constructor(props) {
     //     super(props)
     // }
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
           apiReturn: [],
           artist: '',
@@ -17,30 +17,30 @@ class Tabs1 extends Component {
           
         };
     }
-        componentDidMount() {         
-            fetch("http://www.songsterr.com/a/ra/songs.json?pattern=Beatles")
-                .then(res => res.json())
-                .then(songInfo => {
-                    // this.setState({ apiReturn: [songInfo] })
-                    this.setState({ artist: [songInfo[0].artist.name] });
-                    songInfo.forEach(infoPack => {
-                      this.state.apiReturn.push(infoPack)    
-                    })
-                    songInfo.forEach(infoPack => {
-                        this.state.songs.push(infoPack.title)    
-                    })
-                })
-                console.log(this.state.apiReturn)
-                console.log(this.state.songs)
-                console.log(this.props.songs)
-    }
+    //     componentDidMount() {         
+    //         fetch("http://www.songsterr.com/a/ra/songs.json?pattern=Marley")
+    //             .then(res => res.json())
+    //             .then(songInfo => {
+    //                 // this.setState({ apiReturn: [songInfo] })
+    //                 this.setState({ artist: [songInfo[0].artist.name] });
+    //                 songInfo.forEach(infoPack => {
+    //                   this.state.apiReturn.push(infoPack)    
+    //                 })
+    //                 songInfo.forEach(infoPack => {
+    //                     this.state.songs.push(infoPack.title)    
+    //                 })
+    //             })
+    //             console.log(this.state.apiReturn)
+    //             console.log(this.state.songs)
+    //             console.log(this.props.songs)
+    // }
             
 
 
     render() {
         // console.log(this.props.songs)
         // console.log(this.props.totalRes)
-        const items = this.state.songs.map((song, i ) => {
+        const items = this.props.songs.map((song, i ) => {
         return <li>{song}</li> });
 
             // console.log(items)
