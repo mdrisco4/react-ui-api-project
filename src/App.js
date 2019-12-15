e3f8756d144f7c468e1c610b1247e4d473d5bab5
michaeldriscoll <michaelndriscoll81@gmail.com>
2019-12-14 22:23:07 -0500

    taking a crack at the Marvel API again, if not fetching by mid-day tomorrow going to pick API that works and go with it

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@
 
 import React, { Component } from 'react';
 import './App.css';
-// import Tabs from './Tabs.js';
+import Tabs from './Tabs.js';
 import 'react-tabs/style/react-tabs.css'
 // import { Tab, Tabs, TabList, TabPanel } from 'react-tabs'
 // import Header from './Header.js';
@@ -27,54 +27,81 @@ class App extends Component {
       
     };
 }
+// componentDidMount() {         
+//     fetch("http://www.songsterr.com/a/ra/songs.json?pattern=Marley")
+//         .then(res => res.json())
+//         .then(songInfo => {
+//             // this.setState({ apiReturn: [songInfo] })
+//             this.setState({ artist: [songInfo[0].artist.name] });
+//             songInfo.forEach(infoPack => {
+//               this.state.apiReturn.push(infoPack)    
+//             })
+//             songInfo.forEach(infoPack => {
+//                 this.state.songs.push(infoPack.title)    
+//             })
+              
+//               console.log(songInfo[4].title)
+             
+             
+             
+//               // console.log(songInfo[0].tabTypes[0])
+//            })
+//             // console.log(this.state.apiReturn)
+            
+            
+            
+//             let array = []
+//             this.state.songs.forEach(song => {
+//               array.push(song)
+//             })
+//             console.log(array)
+//             console.log(this.state.songs)
+//             console.log(this.state.songs[8])
+            
+            
+            
+//             // const thing = this.get(['this.state.songs', 11])
+//             // console.log(thing)
+//  }
+
+
+
+
+
 componentDidMount() {         
-    // fetch("http://www.songsterr.com/a/ra/songs.json?pattern=Marley")
-    fetch("http://gateway.marvel.com/v1/public/comics?ts=1&apikey=1234&hash=ffd275c5130566a2916217b101f26150")
+      fetch("http://gateway.marvel.com/v1/public/comics", {
+        "apikey": "69be26d469c729fce76dafc0e7933d34",
+        ts: toString(Date.now()),
+        hash: "b0db4f68e7f76a84c9ee20ab9c63108d",
+        Headers: {Accept: 'application/json'}
+
+          
+      })
         .then(res => res.json())
         .then(songInfo => {
-            // this.setState({ apiReturn: [songInfo] })
-            
-            console.log(songInfo)
-            
-            // this.setState({ artist: [songInfo[0].artist.name] });
-            // songInfo.forEach(infoPack => {
-            //   this.state.apiReturn.push(infoPack)    
-            // })
-            // songInfo.forEach(infoPack => {
-            //     this.state.songs.push(infoPack.title)    
-            // })
-              
-            //   console.log(songInfo[4].title)
-              
-              
-              
-              // console.log(songInfo[0].tabTypes[0])
-           })
-            // console.log(this.state.apiReturn)
-            
-            
-            
-            // let array = []
-            // this.state.songs.forEach(song => {
-            //   array.push(song)
-            // })
-            // console.log(array)
-            // console.log(this.state.songs)
-            // console.log(this.state.songs[8])
-           
-           
-           
-            // console.log(md5(ts + "69be26d469c729fce76dafc0e7933d34" + "82f8e29ecde6f6c5421e8938a8a410da894103e9"))
-            // const thing = this.get(['this.state.songs', 11])
-            // console.log(thing)
- }
+      })
+  }
+        
 
+
+//   componentDidMount() {         
+//     Request Url: "http://gateway.marvel.com/v1/public/comics"
+//     Request Method: GET
+//     Params: {
+//       "apikey": "your api key",
+//       "ts": "a timestamp",
+//       "hash": "your hash"
+//     }
+//     Headers: {Accept: 'application/json'}
+// }
  
  render() {
   //  let music = [...this.state.songs]
   //  console.log(music)
    const items = this.state.songs.map((song, i ) => {
    return <li item={song} key={i}/> });
+
+   console.log(Date.now())
     
   return (
     <div>
@@ -83,9 +110,7 @@ componentDidMount() {
         the
         { items }
       </ul>
-      {/* <Tabs artist={this.state.artist} songs={this.state.songs} totalRes={this.state.apiReturn}/> */}
-     
-     
+      <Tabs artist={this.state.artist} songs={this.state.songs} totalRes={this.state.apiReturn}/>
       {/* <Tab songs={this.state.songs}/> */}
     </div>
     );
